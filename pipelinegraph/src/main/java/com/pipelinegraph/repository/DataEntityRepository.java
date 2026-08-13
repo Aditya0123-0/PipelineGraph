15935ed80dba671f79c7c2d7b1f0c0fa32393bdc
Aditya0123-0 <adityawadkarofficial@gmail.com>
2026-08-13 17:29:50 +0530

    Implemented Repositories

--- pipelinegraph/src/main/java/com/pipelinegraph/repository/DataEntityRepository.java
+++ pipelinegraph/src/main/java/com/pipelinegraph/repository/DataEntityRepository.java
@@ -1,10 +1,9 @@
 package com.pipelinegraph.repository;
 
-
-import com.pipelinegraph.model.SystemNode;
+import com.pipelinegraph.model.DataEntity;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SystemRepository extends Neo4jRepository<SystemNode,String> {
+public interface DataEntityRepository extends Neo4jRepository<DataEntity,String> {
 }
--- pipelinegraph/src/main/java/com/pipelinegraph/repository/PipelineRepository.java
+++ pipelinegraph/src/main/java/com/pipelinegraph/repository/PipelineRepository.java
@@ -1,10 +1,9 @@
 package com.pipelinegraph.repository;
 
-
-import com.pipelinegraph.model.SystemNode;
+import com.pipelinegraph.model.Pipeline;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SystemRepository extends Neo4jRepository<SystemNode,String> {
+public interface PipelineRepository extends Neo4jRepository<Pipeline,String> {
 }
