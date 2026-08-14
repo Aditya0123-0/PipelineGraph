3e664a8ccfd931362f8d3f102ad90c8dc27fa189
Aditya0123-0 <adityawadkarofficial@gmail.com>
2026-08-14 20:27:24 +0530

    Implemented Docker

--- pipelinegraph/src/main/java/com/pipelinegraph/controller/SystemController.java
+++ pipelinegraph/src/main/java/com/pipelinegraph/controller/SystemController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/system")
+@RequestMapping({"/api/system", "/api/systems"})
 public class SystemController {
 
     private final SystemService systemService;
